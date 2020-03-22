f3fb3e388cdc043f162b34a16eef805dfdaff428
Jesuraj77 <isysraju2001@gmail.com>
2020-03-22 14:06:33 +0530

    Code Added

---
+++ ConsumeApiClient/Controllers/CrudController.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ConsumeApiClient.Controllers
+{
+    public class CrudController : Controller
+    {
+       //code here to add new resources and update the resources and  delete the resources.
+       // code here to use sorting and paging by your client and implement HTTP caching in MVC client and implement various version approach in API.
+    }
+}
